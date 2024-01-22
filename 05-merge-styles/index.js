a886fc7929f54bae9ec4d3cbe99476abdef8320d
Ivan Eroshin <itseroshin@gmail.com>
2024-01-22 20:33:06 +0300

    feat: task 5 finished

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,6 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(path.resolve(__dirname, 'styles'), (e, files) => {
-  console.log(files);
+const styleFile = path.resolve(__dirname, 'project-dist', 'bundle.css');
+const styleFolder = path.resolve(__dirname, 'styles');
+
+fs.createWriteStream(path.resolve(styleFile));
+fs.readdir(path.resolve(styleFolder), (e, files) => {
+  for (const file of files) {
+    fs.stat(path.join(styleFolder, file), (e, stats) => {
+      if (stats.isFile() && path.extname(file) === '.css') {
+        const stream = fs.createReadStream(
+          path.resolve(styleFolder, file),
+          'utf-8',
+        );
+        let cssFile = '';
+
+        stream.on('data', (chunk) => (cssFile += chunk));
+        stream.on('end', () => {
+          fs.appendFile(styleFile, cssFile, (e) => {
+            if (e) {
+              throw e;
+            }
+          });
+        });
+      }
+    });
+  }
+  console.log('The BUNDLE.CSS file was updated.');
 });
